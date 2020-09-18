8ef64ad25b87666b6d9c4841b4828c514a6b4077
prndev <prndev@users.noreply.github.com>
2020-09-19 00:48:58 +0200

    Improve keyboard shortcuts.
    Add option for keyboard shortcut to Settings.
    Style visual indicators.
    Do not interfere with text inputs.

--- src/javascript/keyboard_shortcuts.js
+++ src/javascript/keyboard_shortcuts.js
@@ -1,37 +1,48 @@
 window.keyboard_shortcuts = {}
 
 document.addEventListener('keyup', (e) => {
-	let link = window.keyboard_shortcuts[e.key];
-	if (link && e.shiftKey === false) {
+	let link = window.keyboard_shortcuts[e.key.toUpperCase()];
+	let isTyping = e.originalTarget && ['INPUT'].contains(e.originalTarget.tagName);
+	if (link && !isTyping) {
 		link.click();
 	}
 }, false);
 
-$(document).on(':passagedisplay', function (ev) {
-	window.keyboard_shortcuts = {};
-	let keyword = (s) => {
-		// thank you, https://stackoverflow.com/questions/861721/
-		var words = ['go', 'visit', 'to', 'the', 'Wear','Set','Rename','Del','Have','a','Wait','15m','30m','1hr','Detailed','Remove','Insert'];
-		var re = new RegExp('\\b(' + words.join('|') + ')\\b', 'gi');
-		return (s || '').replace(re, '').replace(/[ ]{2,}/, ' ').trim();
-	}
-	let badchars = Array.from('+▼←→↓↑'); // TODO: rather filter everything except A-Z0-9
-	let links = document.getElementById('story').querySelectorAll('a.link-internal');
-	links.forEach(link => {
-		let text = keyword(link.innerText).toUpperCase();
-		text = text.replace(/[^\\x65-\\x90]/g, '');
-		if (text) {
-			let keychars = Array.from(text);
-			let usedchars = Object.keys(window.keyboard_shortcuts);
-			keychars.find(kc => {
-				if (!usedchars.contains(kc)) {
-					window.keyboard_shortcuts[kc] = link;
-					link.innerHTML = `<span class="keyboard_shortcut">${kc}</span>: ${link.innerHTML}`;
-					return true;
-				}
-				return false;
-			});
+let add_keyboard_shortcuts = (ev) => {
+	if (State.active.variables.useKeyboardShortcuts) {
+		window.keyboard_shortcuts = {};
+		let keyword = (s) => {
+			// from https://stackoverflow.com/questions/861721/
+			var words = ['go', 'visit', 'to', 'the', 'Have', 'a'];
+			words = words.concat(['Wear', 'Set', 'Rename', 'Del', 'Detailed', 'Wait', 'Remove', 'Insert', 'Wardrobe', 'Quick', 'Detailed', 'Slot', 'your']);
+			var re = new RegExp('\\b(' + words.join('|') + ')\\b', 'gi');
+			return (s || '').replace(re, '').replace(' ','').trim();
 		}
-	});
-	//console.log(window.keyboard_shortcuts);
-});
+		let links = document.getElementById('story').querySelectorAll('a.link-internal');
+		links.forEach(link => {
+			if (link.classList.contains('macro-click')) {
+				link.addEventListener('click', add_keyboard_shortcuts);
+			}
+			if (link.querySelector('.keyboard_shortcut')) {
+				// already has shortcut assigned
+			} else {
+				let only_typable_characters = link.innerText.toUpperCase().replace(/[^A-Z0-9 ]/g, '');
+				let text = keyword(only_typable_characters);
+				if (text) {
+					let keychars = Array.from(text);
+					let usedchars = Object.keys(window.keyboard_shortcuts);
+					keychars.find(kc => {
+						if (!usedchars.contains(kc)) {
+							window.keyboard_shortcuts[kc] = link;
+							link.innerHTML = `<span class="keyboard_shortcut">${kc}</span> ${link.innerHTML}`;
+							return true;
+						}
+						return false;
+					});
+				}
+			}
+		});
+	}
+}
+
+$(document).on(':passagedisplay', add_keyboard_shortcuts);
