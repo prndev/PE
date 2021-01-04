417d51791d66b0675e8f7f5cfbde90c2a97c1edf
prndev <prndev@users.noreply.github.com>
2021-01-05 00:10:41 +0100

    Added "alternative" variants for traditional therapist scenes.

--- makeHTML.bat
+++ makeHTML.bat
@@ -8,7 +8,7 @@ popd
 :: Window will exit without keypress. Use for general compiling when no errors are present.
 
 :: Run the appropriate compiler for the user's CPU architecture.
-CALL "tweeGo\tweego_win86.exe" -o "PE.html" "src"
+CALL "tweeGo\tweego_win86.exe" -o "PE.html" "src" %*
 
 popd
 ECHO Done
